cde411f42a5228a2a46059a8087ce41138050540
Rafi <rafikhairan2632@gmail.com>
2023-11-29 14:40:20 +0700

    update say hello with parameter name

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/rafikhairan/go-say-hello
+module github.com/rafikhairan/go-say-hello/v2
 
 go 1.21.4
--- say_hello.go
+++ say_hello.go
@@ -1,5 +1,5 @@
 package gosayhello
 
-func SayHello() string {
+func SayHello(name string) string {
 	return "Hello World"
 }
